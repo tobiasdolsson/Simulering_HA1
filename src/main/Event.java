bb0b4ed8d2fd96c900cd95bc962e463416e5fdff
tobiasdolsson <tobias.d.olsson@hotmail.com>
2017-04-10 14:33:55 +0200

    Initial commit

---
+++ src/main/Event.java
@@ -0,0 +1,7 @@
+package main;
+
+class Event{
+	public double eventTime;
+	public int eventType;
+	public Event next;
+}
---
+++ src/main/G.java
@@ -0,0 +1,6 @@
+package main;
+
+public class G{
+	public static final int ARRIVAL = 1, READY = 2, MEASURE = 3;
+	public static double time = 0;
+}
