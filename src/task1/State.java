1bd14d52c55b93c403159f61e98a9eb85d8049f1
tobiasdolsson <tobias.d.olsson@hotmail.com>
2017-04-11 13:48:11 +0200

    More clean up

--- src/task1/State.java
+++ src/task1/State.java
@@ -3,7 +3,7 @@ package task1;
 import java.util.*;
 import java.io.*;
 
-class State{
+class State {
 	public int numberInQueue1 = 0, numberInQueue2 = 0, accumulated1 = 0, accumulated2 = 0, noMeasurements = 0;
 	public int noArrivals1 = 0, noArrivals2 = 0;
 	public int rejected;
@@ -11,55 +11,56 @@ class State{
 
 	Random slump = new Random();
 	SimpleFileWriter W = new SimpleFileWriter("number.m", false);
-	
-	public void TreatEvent(Event x){
-		switch (x.eventType){
-			case G.ARRIVAL_TO_1:{
-				
-				noArrivals1++;
-				//System.out.println(noArrivals1);
-				EventList.InsertEvent(G.ARRIVAL_TO_1, G.time + 1);
-				if (numberInQueue1 < 10){
-					numberInQueue1++;
-				} else {
-					rejected++;
-				}
-				
-				if (numberInQueue1 == 1){
-					EventList.InsertEvent(G.DEPARTURE_FROM_1, G.time - (2.1)*Math.log(slump.nextDouble()));
-				}
 
-			} break;
-			case G.DEPARTURE_FROM_1:{
-				numberInQueue1--;
-				numberInQueue2++;
-				noArrivals2++;
-				if (numberInQueue2 == 1){
-					EventList.InsertEvent(G.DEPARTURE_FROM_2, G.time + 2);
-				}
-				if (numberInQueue1 > 0){
-					EventList.InsertEvent(G.DEPARTURE_FROM_1, G.time - (2.1)*Math.log(slump.nextDouble()));
-				}
-				
-			} break;
-			
-			case G.DEPARTURE_FROM_2:{
-				numberInQueue2--;
-				if (numberInQueue2 > 0){
-					EventList.InsertEvent(G.DEPARTURE_FROM_2, G.time + 2);
-				}
-			} break;
-			
-			case G.MEASURE:{
-				accumulated1 = accumulated1 + numberInQueue1;
-				accumulated2 = accumulated2 + numberInQueue2;
-				//accRejected = accRejected + rejected;
-				noMeasurements++;
-				//System.out.println(G.time);
-				EventList.InsertEvent(G.MEASURE, G.time - (5.0)*Math.log(slump.nextDouble()));
-				//W.println(String.valueOf(numberInQueue1));
-			} break;
-			
+	public void TreatEvent(Event x) {
+		switch (x.eventType) {
+		case G.ARRIVAL_TO_1: {
+
+			noArrivals1++;
+			EventList.InsertEvent(G.ARRIVAL_TO_1, G.time + 1);
+			if (numberInQueue1 < 10) {
+				numberInQueue1++;
+			} else {
+				rejected++;
+			}
+
+			if (numberInQueue1 == 1) {
+				EventList.InsertEvent(G.DEPARTURE_FROM_1, G.time - (2.1) * Math.log(slump.nextDouble()));
+			}
+
+		}
+			break;
+		case G.DEPARTURE_FROM_1: {
+			numberInQueue1--;
+			numberInQueue2++;
+			noArrivals2++;
+			if (numberInQueue2 == 1) {
+				EventList.InsertEvent(G.DEPARTURE_FROM_2, G.time + 2);
+			}
+			if (numberInQueue1 > 0) {
+				EventList.InsertEvent(G.DEPARTURE_FROM_1, G.time - (2.1) * Math.log(slump.nextDouble()));
+			}
+
+		}
+			break;
+
+		case G.DEPARTURE_FROM_2: {
+			numberInQueue2--;
+			if (numberInQueue2 > 0) {
+				EventList.InsertEvent(G.DEPARTURE_FROM_2, G.time + 2);
+			}
+		}
+			break;
+
+		case G.MEASURE: {
+			accumulated1 = accumulated1 + numberInQueue1;
+			accumulated2 = accumulated2 + numberInQueue2;
+			noMeasurements++;
+			EventList.InsertEvent(G.MEASURE, G.time - (5.0) * Math.log(slump.nextDouble()));
+			// W.println(String.valueOf(numberInQueue1));
+		}
+			break;
+
 		}
 	}
 }
--- src/task1/Template2006.java
+++ src/task1/Template2006.java
@@ -6,30 +6,28 @@ import java.io.*;
 //testing commit
 
 public class Template2006 {
- 
-    public static void main(String[] args) throws IOException {
-    	Event actEvent;
-    	State actState = new State();
-    	new EventList();
-        EventList.InsertEvent(G.ARRIVAL_TO_1, 0);
-        EventList.InsertEvent(G.MEASURE, 5);
-        //EventList.InsertEvent(G.MEASURE_2, 5);
-        //EventList.InsertEvent(G.MEASURE2, 5);
-    	while (actState.noMeasurements < 1000){
-    		actEvent = EventList.FetchEvent();
-    		G.time = actEvent.eventTime;
-    		actState.TreatEvent(actEvent);
-    	}
-    	System.out.println(1.0*actState.accumulated1/actState.noMeasurements);
-    	System.out.println("Accumulated: "+actState.accumulated1);
-    	//System.out.println("Accumulated Rejected: "+actState.accRejected);
-    	System.out.println("Measurements: "+actState.noMeasurements);
-    	System.out.println("Arrivals: "+ actState.noArrivals1);
-    	System.out.println("Rejected: "+actState.rejected);
-    	System.out.println("Rejected ratio: "+1.0*actState.rejected/actState.noArrivals1);
-    	System.out.println("Mean in Q2: " + 1.0*actState.accumulated2/actState.noMeasurements);
-    	//System.out.println("Rejected ratio: "+actState.accRejected/actState.accumulated1);
-    	System.out.println("Time: "+G.time);
-    	actState.W.close();
-    }
+
+	public static void main(String[] args) throws IOException {
+		Event actEvent;
+		State actState = new State();
+		new EventList();
+		EventList.InsertEvent(G.ARRIVAL_TO_1, 0);
+		EventList.InsertEvent(G.MEASURE, 5);
+		while (actState.noMeasurements < 1000) {
+			actEvent = EventList.FetchEvent();
+			G.time = actEvent.eventTime;
+			actState.TreatEvent(actEvent);
+		}
+
+		System.out.println("Accumulated: " + actState.accumulated1);
+		System.out.println("Measurements: " + actState.noMeasurements);
+		System.out.println("Time: " + G.time);
+		System.out.println("Arrivals: " + actState.noArrivals1);
+		System.out.println("Rejected: " + actState.rejected);
+		System.out.println("Rejected ratio: " + 1.0 * actState.rejected / actState.noArrivals1);
+		System.out.println("Mean in Q1: " + 1.0 * actState.accumulated1 / actState.noMeasurements);
+		System.out.println("Mean in Q2: " + 1.0 * actState.accumulated2 / actState.noMeasurements);
+
+		actState.W.close();
+	}
 }
