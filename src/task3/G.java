a647d45f1d57213622f4a569c14ec2cd64d4a875
tobiasdolsson <tobias.d.olsson@hotmail.com>
2017-04-26 17:04:32 +0200

    mean time calculation is wrong

--- src/task3/G.java
+++ src/task3/G.java
@@ -3,5 +3,5 @@ package task3;
 public class G{
 	public static final int ARRIVAL_TO_1 = 0, DEPARTURE_FROM_1 = 1, DEPARTURE_FROM_2 = 2, MEASURE = 3;
 	public static double time = 0;
-	public static double measureTime = 5.0, serviceTime2 = 1.0, serviceTime1 = 1.0, arrivalTime = 2.0;
+	public static double measureTime = 5.0, serviceTime2 = 1.0, serviceTime1 = 1.0, arrivalTime = 1.5;
 }
